b66e1feabde4efe217ed6ac8ca3896f6fb195645
Dione Nascimento <morrone.dione@gmail.com>
2025-06-10 22:44:11 -0300

    update main.py

--- Projetos/Histórica de Preços de Combustíveis/main.py
+++ Projetos/Histórica de Preços de Combustíveis/main.py
@@ -2,9 +2,6 @@
 # %%
 import pandas as pd
 combustiveis_2018_01_df = pd.read_csv('ca-2018-01.csv', sep=';')
-display(combustiveis_2018_01_df) 
+display(combustiveis_2018_01_df)
 # %%
 combustiveis_2018_01_df.info()
-
-
-
