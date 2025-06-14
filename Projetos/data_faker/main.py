ed0d723ded156fd20f49add768628c720065de25
Dione Nascimento <morrone.dione@gmail.com>
2025-06-14 11:05:14 -0300

    commit

--- Projetos/data_faker/main.py
+++ Projetos/data_faker/main.py
@@ -1,20 +1,21 @@
 import csv
 from faker import Faker
 
-fake = Faker()
+fake = Faker('pt_BR')  # Configurando o Faker para o português do Brasil
 
 # Criando dados falsos para um banco de dados de usuários
 with open('fakeusers.csv', 'w', newline='') as csvfile:
-    fieldnames = ['nome', 'email', 'data_nascimento', 'endereço', 'telefone']
+    fieldnames = ['nome', 'email', 'data_nascimento', 'estado', 'cidade', 'telefone']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
-    for _ in range(1000):
+    for _ in range(10000):
         writer.writerow({
             'nome': fake.name(),
             'email': fake.email(),
             'data_nascimento': fake.date_of_birth(),
-            'endereço': fake.address(),
+            'estado': fake.state(),
+            'cidade': fake.city(),
             'telefone': fake.phone_number()
 
         })
